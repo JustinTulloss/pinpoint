b35c1adacb5a3c6fa2d5609ba26f3a7e7eb1dfba
Justin Tulloss <justin.tulloss@gmail.com>
2014-05-07 14:57:20 -0700

    Add ability to do a production release

--- Makefile
+++ Makefile
@@ -2,5 +2,16 @@ dev:
 	lein cljsbuild auto &
 	http-server
 
+react:
+	mkdir -p react/externs
+	curl -L http://fb.me/react-0.10.0.min.js > react/react.min.js
+	curl -L https://raw.githubusercontent.com/steida/este-library/master/externs/react.js > react/externs/react.js
+
+release: react
+	lein cljsbuild once release
+
+distclean: clean
+	git clean -xfd
+
 clean:
-	rm -rf out
+	rm -rf out release
--- project.clj
+++ project.clj
@@ -1,13 +1,23 @@
 (defproject pinpoint "0.0.0"
   :plugins [[lein-cljsbuild "1.0.3"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.301.0-deb34a-alpha"]
                  [org.clojure/clojurescript "0.0-2202"]
                  [om "0.6.2"]]
   :cljsbuild {
-    :builds [{
+    :builds [{:id "dev"
              :source-paths ["src"]
              :compiler {
                :output-to "out/main.js"
                :output-dir "out"
                :optimizations :none
-               :source-map true}}]})
+               :source-map true}}
+             {:id "release"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "release/main.js"
+                :output-dir "release"
+                :pretty-print false
+                :optimizations :advanced
+                :preamble ["react/react.min.js"]
+                :externs ["react/externs/react.js"]}}]})
