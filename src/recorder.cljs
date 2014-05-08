4814d101eb2d02f8a5b62d8253b9dcc5b4fb206e
Justin Tulloss <justin.tulloss@gmail.com>
2014-05-08 11:56:55 -0700

    Clicking on the record button now publishes to a channel

--- src/recorder.cljs
+++ src/recorder.cljs
@@ -1,19 +1,30 @@
 (ns recorder
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]))
 
 (defn recorder-button [data owner]
   (reify
-    om/IRender
-    (render [this]
+    om/IRenderState
+    (render-state [this {:keys [record]}]
       (dom/div #js {:className "col-md-12" :style #js {:textAlign "center"}}
-        (dom/button #js {:className "btn btn-lg btn-primary"} (:text data))))))
+        (dom/button #js {
+            :className "btn btn-lg btn-primary"
+            :onClick (fn [e] (put! record []))
+          } (:text data))))))
 
 (def app-state
   (atom
     {:text "Record!"}))
 
-(om/root
-  recorder-button
-  app-state
-  {:target (. js/document (getElementById "my-app"))})
+(let [record-chan (chan)]
+  (go (loop []
+    (let [_ (<! record-chan)]
+      (js/alert "fuck yeah channels!")
+      (recur))))
+  (om/root
+    (fn [app-state owner]
+      (om/build recorder-button app-state {:init-state {:record record-chan}}))
+    app-state
+    {:target (. js/document (getElementById "my-app"))}))
