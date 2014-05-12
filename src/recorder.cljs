90f1f434768ed2b64a82bde1cb70dbca094687ff
Justin Tulloss <justin.tulloss@gmail.com>
2014-05-12 13:22:37 -0700

    Attempt to push positions to a channel
    It doesn't work. If I just return the component in om/root, then it
    works. However, combining it with om/build does not. It throws a
    "Uncaught Error: No protocol method IRenderQueue.-queue-render! defined for
    type null" error.

--- src/recorder.cljs
+++ src/recorder.cljs
@@ -4,30 +4,40 @@
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! chan <!]]))
 
-(defn recorder-button [data owner]
+(enable-console-print!)
+
+(defn recorder-button [app owner]
+  (defn do-record [recorder]
+    (om/set-state! owner :text "Recording\u2026")
+    (.getCurrentPosition (.. js/navigator -geolocation) (fn [position]
+      ; XXX: There should be some way to just revert to the old state here...
+      (om/set-state! owner :text "Record!")
+      (put! recorder position))))
   (reify
+    m/IDisplayName
+    (display-name [_] "RecorderButton")
+    om/IInitState
+    (init-state [_]
+      {:text "Record!"})
     om/IRenderState
-    (render-state [this {:keys [record]}]
+    (render-state [_ {:keys [recorder text]}]
       (dom/div #js {:className "col-md-12" :style #js {:textAlign "center"}}
         (dom/button #js {
             :className "btn btn-lg btn-primary"
-            :onClick (fn [e] (put! record []))
-          } (:text data))))))
-
-(def app-state
-  (atom
-    {:text "Record!"}))
+            :onClick #(do-record recorder)
+          } text)))))
 
 (let [record-chan (chan)]
   (go (loop []
-    (let [_ (<! record-chan)]
-      (js/alert "fuck yeah channels!")
+    (let [position (<! record-chan)]
+      (.log js/console (.stringify js/JSON position))
       (recur))))
   (om/root
     (fn [app-state owner]
       (cond
-       (aget js/navigator "geolocation") (om/build recorder-button app-state {
-         :init-state {:record record-chan}})
-       :else (dom/h1 nil #js {:text "Please use a browser that supports geolocation"})))
-    app-state
+       (. js/navigator -geolocation) (om/build recorder-button nil {
+         :state {:recorder record-chan}})
+       :else (dom/h1 nil "Please use a browser that supports geolocation")))
+    nil
     {:target (. js/document (getElementById "my-app"))}))
+
